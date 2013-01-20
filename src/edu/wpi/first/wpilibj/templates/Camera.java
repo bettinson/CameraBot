8c0431d028c5581d0a5436e481f7fee225c809e2
Matt Bettinson <mattbettinson@Matthews-MacBook-Pro.local>
2013-01-20 13:14:54 -0500

    Added feature of being able to center on the designated color

--- src/edu/wpi/first/wpilibj/templates/Camera.java
+++ src/edu/wpi/first/wpilibj/templates/Camera.java
@@ -19,74 +19,60 @@ import edu.wpi.first.wpilibj.image.ParticleAnalysisReport;
  * @author mattbettinson
  */
 public class Camera {
-        AxisCamera camera;
+
+	AxisCamera camera;
 	ParticleAnalysisReport[] orderedParticles;
 	ParticleAnalysisReport first;
 	int firstsWidth, pixelCentre, close;
 	AnalogChannel ultraSonic;
 	ParticleAnalysisReport largestParticle;
-        Relay relay;
+	Relay relay;
+	BinaryImage binaryImage;
 
-        public Camera ()
-        {
+	public Camera() {
 		camera = AxisCamera.getInstance();
 		camera.writeBrightness(50);
 		relay = new Relay(Config.LIGHTS);
 		relay.setDirection(Relay.Direction.kReverse);
 	}
-	
-	public void getLargestParticle(int[] imageValues)
-	{
-		try
-		{
+
+	public ParticleAnalysisReport[] getLargestParticle(int[] imageValues) {
+		try {
 			relay.set(Relay.Value.kOn);
 			ColorImage colorImage = camera.getImage();
 			relay.set(Relay.Value.kOff);
-			BinaryImage binaryImage = colorImage.thresholdRGB(imageValues[0],imageValues[1],imageValues[2],imageValues[3],imageValues[4],imageValues[5]);
+			binaryImage = colorImage.thresholdRGB(imageValues[0], imageValues[1], imageValues[2], imageValues[3], imageValues[4], imageValues[5]);
 			colorImage.free();
-                        binaryImage = binaryImage.removeSmallObjects(true, 1);
+			binaryImage = binaryImage.removeSmallObjects(true, 1);
 			binaryImage = binaryImage.convexHull(true);
-			
+
 			orderedParticles = binaryImage.getOrderedParticleAnalysisReports();
 			binaryImage.free();
-			largestParticle = orderedParticles[0];	
-                        System.out.println(largestParticle.center_mass_x);
-                        System.out.println(largestParticle.center_mass_y);
+			largestParticle = orderedParticles[0];
+			System.out.println(largestParticle.center_mass_x);
+			System.out.println(largestParticle.center_mass_y);
+		} catch (AxisCameraException ex) {
+			ex.printStackTrace();
+		} catch (NIVisionException ex) {
+			ex.printStackTrace();
 		}
 		
-		catch (AxisCameraException ex)
-		{
-			ex.printStackTrace();
-		}
-		catch (NIVisionException ex)
-		{
-			ex.printStackTrace();
-		}
+		return orderedParticles;
 	}
-	
-	public String leftOrRight()
-	{
-		if (largestParticle.center_mass_x < camera.getResolution().width/2 + 10) 
-		{
+
+	public String leftOrRight() {
+		if (largestParticle.center_mass_x < camera.getResolution().width / 2 + 10) {
 			return "right";
-		}
-			
-		else if (largestParticle.center_mass_x > camera.getResolution().width/2 - 10) 
-		{
+		} else if (largestParticle.center_mass_x > camera.getResolution().width / 2 - 10) {
 			return "left";
-		} 
-		
-		else if (largestParticle.center_mass_x >= camera.getResolution().width/2 + 10 || largestParticle.center_mass_x <= camera.getResolution().width/2 - 10) 
-		{
+		} else if (largestParticle.center_mass_x >= camera.getResolution().width / 2 + 10 || largestParticle.center_mass_x <= camera.getResolution().width / 2 - 10) {
 			return "centre";
 		}
 		return "nil, yo.";
 	}
-	
-	public void takePicture(int[] values)
-	{
-		try
-		{
+
+	public void takePicture(int[] values) {
+		try {
 			ColorImage img = camera.getImage();
 			BinaryImage bin = img.thresholdRGB(values[0], values[1], values[2], values[3], values[4], values[5]);
 			img.free();
@@ -94,27 +80,23 @@ public class Camera {
 			bin = bin.convexHull(true);
 			bin.write("/testMattISSIRI.png");
 			bin.free();
-                        
-		}
-		catch (NIVisionException ex)
-		{
+
+		} catch (NIVisionException ex) {
 			ex.printStackTrace();
-		}
-		catch (AxisCameraException ex)
-		{
+		} catch (AxisCameraException ex) {
 			ex.printStackTrace();
 		}
 	}
-        
-    public void getNormalPicture() {
-        try {
-            ColorImage img = camera.getImage();
-            img.write("/testMattIsGood.png");
-            img.free();
-        } catch (NIVisionException ex) {
-            ex.printStackTrace();
-        } catch (AxisCameraException ex) {
-            ex.printStackTrace();
-        }
-    }
+
+	public void getNormalPicture() {
+		try {
+			ColorImage img = camera.getImage();
+			img.write("/testMattIsGood.png");
+			img.free();
+		} catch (NIVisionException ex) {
+			ex.printStackTrace();
+		} catch (AxisCameraException ex) {
+			ex.printStackTrace();
+		}
+	}
 }
--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -4,11 +4,10 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
 
-
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.image.ParticleAnalysisReport;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -17,41 +16,70 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-
 public class RobotTemplate extends IterativeRobot {
-    
-    Camera cam;
-    boolean bool = false;
-    
-    public void robotInit() {
-        cam = new Camera();
-         
-    }
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
+	int sleepForMili = 500;
+	Camera cam;
+	boolean bool = false;
+	Bundle leftSide = new Bundle(1, 2);
+	Bundle rightSide = new Bundle(3, 4);
 
-    }
+	public void robotInit() {
+		cam = new Camera();
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        if (!bool) {
-            int[] array = {16, 53, 0, 60, 36, 102};
-            //cam.getLargestParticle(array);
-            cam.getNormalPicture();
-            bool = true;
-        }
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    
-    }
-    
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		int[] array = {202, 255, 86, 207, 0, 255};
+		ParticleAnalysisReport[] orderedParticles;
+		orderedParticles = cam.getLargestParticle(array);
+
+		System.out.println("Amount of particles:" + orderedParticles.length);
+		System.out.println("The largest particle's center x mass:" + orderedParticles[0].center_mass_x);
+		System.out.println("The largest particle's center y mass:" + orderedParticles[0].center_mass_y);
+
+		//cam.getNormalPicture();
+		bool = true;
+
+		String lr = cam.leftOrRight();
+
+		if (lr == "left") {
+			rightSide.set(0.2);
+			try {
+				Thread.sleep(sleepForMili);
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+			rightSide.set(0);
+		} else if (lr == "right") {
+			leftSide.set(0.2);
+			try {
+				Thread.sleep(sleepForMili);
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+			leftSide.set(0);
+		} else if (lr == "center") {
+			System.out.println("YEEEEEE");
+			leftSide.set(0);
+			rightSide.set(0);
+		}
+
+
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+	}
 }
